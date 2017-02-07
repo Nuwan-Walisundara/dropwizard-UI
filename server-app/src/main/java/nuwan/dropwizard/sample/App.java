77572c8d5fb8187539ba809ca031c08344674199
Nuwan Walisundara <nuwan@wso2telco.com>
2017-02-07 11:26:24 +0530

    initial commit

---
+++ server-app/src/main/java/nuwan/dropwizard/sample/App.java
@@ -0,0 +1,13 @@
+package nuwan.dropwizard.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
